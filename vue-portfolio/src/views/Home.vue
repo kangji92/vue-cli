3f5769151a487f6d0b287d3f9e03e5394e4a317d
kangji92 <jykang6731@gmail.com>
2022-02-09 14:06:26 +0900

    add project

---
+++ vue-portfolio/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div class="intro">
+      <div class="intro-inner">
+        <div class="ttl-group">
+          <h2>
+            Web Publisher<br />
+            <em><b>Kang</b> JI Yeon</em>
+          </h2>
+        </div>
+        <ul class="intro__btns-wrap">
+          <li>
+            <router-link tag="button" class="btn trn" to="/portfolio">
+              Portfolio
+            </router-link>
+          </li>
+          <li>
+            <router-link tag="button" class="btn navy" to="/librarys"
+              >Librarys</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ vue-portfolio/src/views/LibrarysPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="library-main">
+    <p>library</p>
+  </div>
+</template>
+
+<script>
+//import { SidebarMenu } from 'vue-sidebar-menu'
+
+export default {};
+</script>
